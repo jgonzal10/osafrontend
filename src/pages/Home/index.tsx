9bfe90edd1e17c923823bf5576c4972b88b4c3cc
jgonzal10 <jgonzal10@gmail.com>
2025-04-07 16:24:55 +0200

    issues

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,41 +16,6 @@ const Home = () => {
   const { t } = useTranslation("app");
   {t("home.sidenote")}
   const issues:Issue[] = t("home.issues", { returnObjects: true });
-  const issues2 = [
-    {
-      icon: "🐞",
-      title:
-        'Console error: Warning: Each child in a list should have a unique "key" prop.',
-      description:
-        "Hope you are able to find what is causing this error, as it is annoying.",
-    },
-    {
-      icon: "🐞",
-      title:
-        'The word "known" should be displayed bold in the introduction text.',
-      description:
-        "When implementing a solution, please ensure to not change the i18n text.",
-    },
-    {
-      icon: "🐞",
-      title:
-        "User avatar in app bar is missing, although user should be fetched on app start correctly.",
-      description:
-        "On app start we load the current user object via a MobX store, but for any reason the user avatar is not displayed in the top right of the app bar. Attention: When solving this issue, you might will be confronted with a second bug.",
-    },
-    {
-      icon: "🐞",
-      title: "Optional: Countdown is broken sometimes (hard to reproduce).",
-      description:
-        "Some developers mentioned that the countdown in the app header behaves strange sometimes, but unfortunately they were not able to reproduce this glitch reliably, maybe you find the root cause.",
-    },
-    {
-      icon: "⭐️",
-      title: "Optional: It would be great to be able to switch the language.",
-      description:
-        "Please add a language select control in the app bar to swicth the UI language between english and german.",
-    },
-  ];
 
   return (
     <Box p={2} maxHeight="calc(100vh - 64px)" overflow={["auto", "auto"]}>
